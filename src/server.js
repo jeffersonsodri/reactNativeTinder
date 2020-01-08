1094b19ff32637410a2e29cd616ba145b9dded06
Jefferson Souza Rodrigues <jsodricacula@hotmail.com>
2020-01-08 17:23:31 -0400

    Add Mongoose to aplication

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const server =  express();
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
-const routes = require('./router');
 
+mongoose.connect('mongodb+srv://tinder:tinder123@cluster0-7uu3i.mongodb.net/tinder?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
