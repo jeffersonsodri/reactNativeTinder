4db6ea97f9c0b0335bf897dd81f0ffb211ffeaf6
Jefferson Souza Rodrigues <jsodricacula@hotmail.com>
2020-01-12 12:08:47 -0400

    Initial of application to frontend in language React

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/css/Login.css';
+
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <h3>Tinder</h3>
+        
+        </div>
+    );
+
+}
+
